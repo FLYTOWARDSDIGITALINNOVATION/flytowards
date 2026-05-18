ad5657f7601907dc2e1e97d8cd8bfe34bf9c8597
lathavairamuthu1004-crypto <lathavairamuthu.1004@gmail.com>
2026-05-18 22:28:00 +0530

    feat: migrate FlyTowards to isolated port 5003

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 export const API_BASE_URL = isLocal 
-    ? 'http://localhost:5002/api' 
+    ? 'http://localhost:5003/api' 
     : 'https://flytowardsdigitalinnovation.com/ft-api';
 
 export const IMAGE_BASE_URL = isLocal 
-    ? 'http://localhost:5002' 
+    ? 'http://localhost:5003' 
     : 'https://flytowardsdigitalinnovation.com/ft-uploads';
