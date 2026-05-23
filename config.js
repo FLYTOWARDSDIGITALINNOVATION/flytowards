51f9c2df439d91e10729ca20d26e1fba76a32936
I.Udhaya Banu <udhayabanu2005@gmail.com>
2026-05-23 11:08:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// Central configuration for dynamic environment switching
+const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+
+export const API_BASE_URL = isLocal 
+    ? 'http://localhost:5010/api' 
+    : 'https://flytowardsdigitalinnovation.com/ft-api';
+
+export const IMAGE_BASE_URL = isLocal 
+    ? 'http://localhost:5010' 
+    : 'https://flytowardsdigitalinnovation.com/ft-uploads';
