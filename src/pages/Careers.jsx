03f4baf046cc14cb07ec5fdd33dad5569e562430
FLYTOWARDSDIGITALINNOVATION <flytowardsdigitalinnovation@gmail.com>
2026-06-06 10:44:11 +0530

    Update canonical URL in Careers page

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -20,7 +20,7 @@ const Careers = () => {
 
         const canonical = document.querySelector('link[rel="canonical"]');
         if (canonical) canonical.setAttribute("href",
-            "https://flytowardsdigitalinnovation.com/careers/");
+            "https://flytowardsdigitalinnovation.com/careers");
 
     }, []);
 
