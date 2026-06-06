a71b7f62c2beba58f6933f0f74d3ee23ca012e23
FLYTOWARDSDIGITALINNOVATION <flytowardsdigitalinnovation@gmail.com>
2026-06-06 10:43:25 +0530

    Update canonical link for About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
 
         const canonical = document.querySelector('link[rel="canonical"]');
         if (canonical) canonical.setAttribute("href",
-            "https://flytowardsdigitalinnovation.com/about/");
+            "https://flytowardsdigitalinnovation.com/about");
 
     }, []);
     return (
